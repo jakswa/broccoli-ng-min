0334d47409c4796f24f68dac3d77a3227f47d550
Jake Swanson <jakswa@gmail.com>
2014-02-17 21:46:50 -0500

    initial commit
    pretty much ripped off the broccoli-coffee filter

---
+++ README.md
@@ -0,0 +1,16 @@
+# broccoli-ng-min
+
+A ng-min filter for Broccoli.
+
+## Installation
+
+```bash
+npm install --save broccoli-ng-min
+```
+
+## Usage
+
+```js
+var filterNgMin = require('broccoli-ng-min');
+tree = filterNgMin(tree, options);
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+var Filter = require('broccoli-filter')
+
+module.exports = NgMinFilter
+NgMinFilter.prototype = Object.create(Filter.prototype)
+NgMinFilter.prototype.constructor = NgMinFilter
+function NgMinFilter (inputTree, options) {
+  if (!(this instanceof NgMinFilter)) return new NgMinFilter(inputTree, options)
+  this.inputTree = inputTree
+  this.options = options || {}
+}
+
+NgMinFilter.prototype.extensions = ['ngmin']
+NgMinFilter.prototype.targetExtension = 'js'
+
+NgMinFilter.prototype.processString = function (string) {
+  return require('ngmin').annotate(string)
+}
